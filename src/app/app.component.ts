d30a2ee7f855fcc2c379274f7d198e1ccdb7a477
davidmikulik <david.mikulik72@gmail.com>
2020-02-17 14:47:19 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-3import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,5 @@
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Hovno';
 }
